63aa45eb0be5eb5da16357bba73403ddbaa5db7b
AnnaNik334743 <annanikiforova564@gmail.com>
2022-08-20 15:26:59 +0300

    new ones

---
+++ acmp/54.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+mins = [float('inf') for _ in range(n)]
+maxes = [float('-inf') for _ in range(m)]
+for i in range(n):
+    row = list(map(int, input().split()))
+    for j, elem in enumerate(row):
+        if elem < mins[i]:
+            mins[i] = elem
+        if elem > maxes[j]:
+            maxes[j] = elem
+print(max(mins), min(maxes))
